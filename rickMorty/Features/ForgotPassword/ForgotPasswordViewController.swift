7d4e9b7365bcd4df6fcb46c140c5b191f0a8f5f8
Lorena1803 <lorena01ambrosio@gmail.com>
2023-02-08 16:30:14 -0300

    criando tela de esqueceu a senha e configurando elementos

---
+++ rickMorty/Features/ForgotPassword/ForgotPasswordViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ForgotPasswordViewController.swift
+//  rickAndMorty
+//
+//  Created by Lorena on 07/02/23.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+    
+    var screen: ForgotPasswordScreen?
+    
+    override func loadView() {
+        screen = ForgotPasswordScreen()
+        view = screen
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        screen?.delegate(delegate: self)
+       
+        view.backgroundColor = .black
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+
+}
+
+//MARK: -Extensão
+
+extension ForgotPasswordViewController: ForgotPasswordDelegate {
+    func tappedBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+}
