e67cd4d4833391b809a4582d2a60ac3552b7ace7
FranklinSolano <franklinsolano40@gmail.com>
2023-03-16 15:46:38 -0300

    alteracao de imagem

--- rickMorty/Features/Profile/ViewController/ProfileVC.swift
+++ rickMorty/Features/Profile/ViewController/ProfileVC.swift
@@ -10,15 +10,23 @@ import UIKit
 class ProfileVC: UIViewController {
     
     var screen: ProfileScreen?
+    var alert: Alert?
+    let imagePicker: UIImagePickerController = UIImagePickerController()
     
     override func loadView() {
         screen = ProfileScreen()
         view = screen
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         screen?.delegate(delegate: self)
+        alert = Alert(controller: self)
+        configImagePicker()
+    }
+    
+    func configImagePicker(){
+        imagePicker.delegate = self
     }
     
 }
@@ -27,11 +35,36 @@ class ProfileVC: UIViewController {
 
 extension ProfileVC: ProfileScreenProtocol {
     func actionEditButton() {
-        
+        self.alert?.alertEditPhoto(completion: { option in
+            switch option {
+            case .camera:
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true)
+                
+            case .library:
+                self.imagePicker.sourceType = .photoLibrary
+                self.present(self.imagePicker, animated: true)
+                
+            case .cancel:
+                break
+            }
+        })
     }
-    
+
     func actionEndButton() {
         let vc:LoginViewController = LoginViewController()
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+// MARK: Extendion UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            self.screen?.imageProfile.image = image
+        }
+        picker.dismiss(animated: true)
     }
 }
