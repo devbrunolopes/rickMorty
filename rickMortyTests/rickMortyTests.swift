427a5c2fa782dd81aeb27430bb6565765496b9c1
FranklinSolano <franklinsolano40@gmail.com>
2023-05-02 15:59:20 -0300

    adicionando testUnitarios ao projeto

---
+++ rickMortyTests/rickMortyTests.swift
@@ -0,0 +1,24 @@
+//
+//  rickMortyTests.swift
+//  rickMortyTests
+//
+//  Created by Franklin  Stilhano on 5/2/23.
+//
+@testable import rickMorty
+import XCTest
+
+class rickMortyTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAgoraVai(){
+        
+    }
+
+}
